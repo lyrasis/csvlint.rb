b919d0086571b447d9f0a72ba97eb0c520d8b7ba
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-09-05 19:22:43 -0400

    noblame: Whitespace and/or formatting changes only

--- lib/csvlint.rb
+++ lib/csvlint.rb
@@ -1,24 +1,24 @@
-require "csv"
-require "date"
-require "open-uri"
-require "tempfile"
-require "typhoeus"
+require 'csv'
+require 'date'
+require 'open-uri'
+require 'tempfile'
+require 'typhoeus'
 
 require "active_support/all"
 require "open_uri_redirections"
 require "uri_template"
 
-require "csvlint/error_message"
-require "csvlint/error_collector"
-require "csvlint/validate"
-require "csvlint/field"
+require 'csvlint/error_message'
+require 'csvlint/error_collector'
+require 'csvlint/validate'
+require 'csvlint/field'
 
-require "csvlint/csvw/metadata_error"
-require "csvlint/csvw/number_format"
-require "csvlint/csvw/date_format"
-require "csvlint/csvw/property_checker"
-require "csvlint/csvw/column"
-require "csvlint/csvw/table"
-require "csvlint/csvw/table_group"
+require 'csvlint/csvw/metadata_error'
+require 'csvlint/csvw/number_format'
+require 'csvlint/csvw/date_format'
+require 'csvlint/csvw/property_checker'
+require 'csvlint/csvw/column'
+require 'csvlint/csvw/table'
+require 'csvlint/csvw/table_group'
 
-require "csvlint/schema"
+require 'csvlint/schema'
